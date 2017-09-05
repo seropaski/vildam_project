ac8ead65091406e38e316f3ce520c821fbe82596
user <user@local.com>
2017-09-05 15:46:25 +0300

    Some improvements

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
+import re
+from collections import defaultdict
 from os import listdir
 from os.path import isfile, join
 
-from collections import defaultdict
+PATH = 'list/'
+DEFAULT_LOGIN = 'root'
+DEFAULT_PASSWORD = 'password'
 
 
-def main():
-    my_path = 'list/'
-    files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-    schema = defaultdict(list)
+def get_full_schema(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+
+    full_schema = defaultdict(list)
     for owner in files:
-        schema[owner] = [dict([('ip', s.replace('\n', '').split(':')[0]),
-                               ('login', s.replace('\n', '').split(':')[1]),
-                               ('password', s.replace('\n', '').split(':')[2])]) for s in
-                         open(join(my_path, owner)).readlines()]
-    print('\nList of owners: %s' % [i for i in schema.keys()])
-    print('\nList of server by owner: %s' % schema['OWNER1'])
-    print('\nList of server info by owner:')
-    print(*['\tServer:\n\t\tip: %s\n\t\tlogin: %s\n\t\tpassword: %s\n' % (server['ip'], server['login'], server['password']) for server in
-            schema['OWNER1']])
-    print('\nWhole schema\n%s' % schema)
+        full_schema[owner] = [dict([
+            ('desc', s.rstrip().split(':')[0]),
+            ('ip', s.rstrip().split(':')[1]),
+            ('login', s.rstrip().split(':')[2] if s.rstrip().split(':')[2] else DEFAULT_LOGIN),
+            ('password', s.rstrip().split(':')[3] if s.rstrip().split(':')[3] else DEFAULT_PASSWORD),
+        ]) for s in
+            open(join(path, owner)).readlines()]
+
+    return full_schema
+
+
+def search_server_by_schema(schema_by_owner, part_of_name):
+    try:
+        pattern = re.compile(part_of_name)
+    except Exception as e:
+        return 'Wrong format of pattern: %s' % e
+
+    server_list = [server for server in schema_by_owner if re.search(pattern, server.get('desc'))]
+
+    return server_list
+
 
 if __name__ == '__main__':
-    main()
+
+    part_of_name_for_search = 'bulk'
+    owner_name = 'OWNER1'
+
+    schema = get_full_schema(PATH)
+    print('Full schema: %s\n' % schema)
+    found_servers = search_server_by_schema(schema[owner_name], part_of_name_for_search)
+    print('Result of search: %s' % found_servers)
